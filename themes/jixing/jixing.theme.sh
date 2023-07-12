160e05826f0cb73c74ad6986e5d44dd03b4ec9ed
Jiaxu Xing <jiaxu.xing@outlook.com>
2023-07-13 01:18:29 +0200

    Fix some blank space issues

--- themes/jixing/jixing.theme.sh
+++ themes/jixing/jixing.theme.sh
@@ -23,13 +23,14 @@
 
 SCM_NONE_CHAR=''
 SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_brown}✗"
-SCM_THEME_PROMPT_CLEAN=""
+SCM_THEME_PROMPT_CLEAN=" ${_omb_prompt_green}✔"
 SCM_THEME_PROMPT_PREFIX="${_omb_prompt_navy}|"
-SCM_THEME_PROMPT_SUFFIX="${_omb_prompt_navy}|"
+SCM_THEME_PROMPT_SUFFIX="${_omb_prompt_navy}| "
+SCM_THEME_PROMPT_FORMAT="${SCM_THEME_PROMPT_PREFIX}%s${SCM_THEME_PROMPT_SUFFIX} "
 SCM_GIT_SHOW_MINIMAL_INFO=true
 
-OMB_PROMPT_VIRTUALENV_FORMAT='(%s)'
-OMB_PROMPT_CONDAENV_FORMAT='(%s)'
+OMB_PROMPT_VIRTUALENV_FORMAT='(%s) '
+OMB_PROMPT_CONDAENV_FORMAT='(%s) '
 
 OMB_PROMPT_SHOW_PYTHON_VENV=${OMB_PROMPT_SHOW_PYTHON_VENV:=true}
 
@@ -52,7 +53,7 @@ function _omb_theme_PROMPT_COMMAND() {
     # Append new history lines to history file
     history -a
 
-    PS1="$python_venv ${hostname} ${_omb_prompt_green}\w $(scm_prompt_char_info)${ret_status}${_omb_prompt_normal} "
+    PS1="$python_venv${hostname} ${_omb_prompt_green}\w $(scm_prompt_char_info)${ret_status}${_omb_prompt_normal}"
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
